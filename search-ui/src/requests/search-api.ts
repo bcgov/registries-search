33889ac7206ea09ad6b8b9d54fd29c2facb8d616
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2022-05-10 13:19:52 -0700

    AutoSuggest API Integration

--- search-ui/src/requests/search-api.ts
+++ search-ui/src/requests/search-api.ts
@@ -2,16 +2,16 @@ import { axios } from '@/utils'
 
 // Interfaces
 import { SearchResponseI, SuggestionResponseI } from '@/interfaces'
-import { BusinessStatuses, BusinessTypes, SuggestionTypes } from '@/enums'
+import { BusinessStatuses, BusinessTypes } from '@/enums'
 
-//const HttpStatus = require('http-status-codes')
+const AUTO_SUGGEST_RESULT_SIZE = 20
 
 export async function getAutoComplete(searchValue: string): Promise<any> {
-  if (!searchValue) return
-  return mockAutoCompleteResponse
-  /*const url = sessionStorage.getItem('SEARCH_API_URL')
+  if (!searchValue) return   
+  const url = sessionStorage.getItem('SEARCH_API_URL')
   const config = { baseURL: url }
-  return axios.get<AutoCompleteResponseIF>(`search/autocomplete?q=${searchValue}`, config)
+  return axios.get<SuggestionResponseI>(`/search/suggest?query=${searchValue}&max_results=${AUTO_SUGGEST_RESULT_SIZE}`,
+   config)
     .then(response => {
       const data = response?.data
       if (!data) {
@@ -20,7 +20,7 @@ export async function getAutoComplete(searchValue: string): Promise<any> {
       return data
     }).catch(error => {
       return error
-    })*/
+    })
 }
 
 export async function searchBusiness(searchValue: string): Promise<any> {
@@ -72,51 +72,6 @@ const mockSearchResponse: SearchResponseI = {
       bn: '1234567895',
       type: BusinessTypes.SOLE_PROPRIETOR,
       status: BusinessStatuses.ACTIVE
-    }
-  ]
-}
-
-const mockAutoCompleteResponse: SuggestionResponseI = {
-  results: [
-    {
-      type: SuggestionTypes.NAME,
-      value: '<b>LA LA</b> CONSTRUCTION'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: 'BREW-<b>LA-LA</b>'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: '<b>LA-LA</b> CREATIONS'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: 'SHOE <b>LA LA</b>'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: '<b>LA LA</b> LA WORLD PRODUCTIONS INC.'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: '<b>LA LA</b> LA AROMATIC BATHING COMPANY'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: 'LA LA LAND ENTERPRISES'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: 'OH LA LA FASHIONS'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: 'MOO LA LA MUSIC'
-    },
-    {
-      type: SuggestionTypes.NAME,
-      value: 'LA LA BAND'
     }
   ]
 }
