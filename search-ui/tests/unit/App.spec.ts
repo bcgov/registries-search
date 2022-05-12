a2266ffc109fe88f1a87addde456b08483911beb
Kial Jinnah <kialjinnah@gmail.com>
2022-05-12 12:06:25 -0700

    added basic jest test on App
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- search-ui/tests/unit/App.spec.ts
+++ search-ui/tests/unit/App.spec.ts
@@ -1,6 +1,24 @@
+// External
+import { mount } from '@vue/test-utils'
+// Local
+import App from '@/App.vue'
+// import vuetify from '@/plugins/vuetify'
+import store from '@/store'
+
+
 // FUTURE: replace this with actual tests on App.vue
 describe('App tests', () => {
+  const wrapper = mount(App, {
+    global: {
+      // plugins: [vuetify],
+      provide: {
+        store: store
+      },
+    },
+    shallow: true  // stubs out children components
+  })
   it('runs', () => {
+    expect(wrapper.vm.isJestRunning).toBe(true)
     expect(1).toBe(1)
   })
 })
