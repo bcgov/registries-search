10d8ce7e968e5a847d2ce3f05817061adc629f30
Kial Jinnah <kialjinnah@gmail.com>
2022-05-16 17:00:07 -0700

    finished pytests for search/suggest pieces
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- search-api/tests/unit/services/test_solr/test_solr_basic.py
+++ search-api/tests/unit/services/test_solr/test_solr_basic.py
@@ -19,7 +19,7 @@ import pytest
 from flask import Flask
 
 from search_api.services import solr
-from search_api.services.solr import SolrDoc, SolrField
+from search_api.services.solr import Solr, SolrDoc, SolrField
 
 from tests import integration_solr
 
@@ -67,4 +67,44 @@ def test_solr_create_delete(test_name, identifier, state, name, legal_type, bn):
     docs = solr.select(f'q={SolrField.IDENTIFIER_SELECT}:{identifier}', 1)
     assert len(docs) == 0
 
-# TODO: test build_split_query, highlight_names, parse_facets
+
+@pytest.mark.parametrize('test_name,params,expected', [
+    ('test-basic-basic', {'query': 'name', 'fields': [SolrField.NAME_SELECT], 'wild': []}, f'q={SolrField.NAME_SELECT}:name'),
+    ('test-basic-basic-wild', {'query': 'name', 'fields': [SolrField.NAME_SELECT], 'wild': [SolrField.NAME_SELECT]}, f'q={SolrField.NAME_SELECT}:name*'),
+    ('test-basic-multi', {'query': 'name', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': []}, f'q={SolrField.NAME_SELECT}:name OR {SolrField.NAME_STEM_AGRO}:name'),
+    ('test-basic-multi-wild-1', {'query': 'name', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': [SolrField.NAME_SELECT]}, f'q={SolrField.NAME_SELECT}:name* OR {SolrField.NAME_STEM_AGRO}:name'),
+    ('test-basic-multi-wild-2', {'query': 'name', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': [SolrField.NAME_STEM_AGRO]}, f'q={SolrField.NAME_SELECT}:name OR {SolrField.NAME_STEM_AGRO}:name*'),
+    ('test-basic-multi-wild-3', {'query': 'name', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO]}, f'q={SolrField.NAME_SELECT}:name* OR {SolrField.NAME_STEM_AGRO}:name*'),
+    ('test-multi-basic', {'query': 'name1 name2 name3', 'fields': [SolrField.NAME_SELECT], 'wild': []}, f'q={SolrField.NAME_SELECT}:name1&fq={SolrField.NAME_SELECT}:name2&fq={SolrField.NAME_SELECT}:name3'),
+    ('test-multi-basic-wild', {'query': 'name1 name2 name3', 'fields': [SolrField.NAME_SELECT], 'wild': [SolrField.NAME_SELECT]}, f'q={SolrField.NAME_SELECT}:name1*&fq={SolrField.NAME_SELECT}:name2*&fq={SolrField.NAME_SELECT}:name3*'),
+    ('test-multi-multi', {'query': 'name1 name2 name3', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': []}, f'q={SolrField.NAME_SELECT}:name1 OR {SolrField.NAME_STEM_AGRO}:name1&fq={SolrField.NAME_SELECT}:name2 OR {SolrField.NAME_STEM_AGRO}:name2&fq={SolrField.NAME_SELECT}:name3 OR {SolrField.NAME_STEM_AGRO}:name3'),
+    ('test-multi-multi-wild-1', {'query': 'name1 name2 name3', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': [SolrField.NAME_SELECT]}, f'q={SolrField.NAME_SELECT}:name1* OR {SolrField.NAME_STEM_AGRO}:name1&fq={SolrField.NAME_SELECT}:name2* OR {SolrField.NAME_STEM_AGRO}:name2&fq={SolrField.NAME_SELECT}:name3* OR {SolrField.NAME_STEM_AGRO}:name3'),
+    ('test-multi-multi-wild-2', {'query': 'name1 name2 name3', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': [SolrField.NAME_STEM_AGRO]}, f'q={SolrField.NAME_SELECT}:name1 OR {SolrField.NAME_STEM_AGRO}:name1*&fq={SolrField.NAME_SELECT}:name2 OR {SolrField.NAME_STEM_AGRO}:name2*&fq={SolrField.NAME_SELECT}:name3 OR {SolrField.NAME_STEM_AGRO}:name3*'),
+    ('test-multi-multi-wild-3', {'query': 'name1 name2 name3', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO], 'wild': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO]}, f'q={SolrField.NAME_SELECT}:name1* OR {SolrField.NAME_STEM_AGRO}:name1*&fq={SolrField.NAME_SELECT}:name2* OR {SolrField.NAME_STEM_AGRO}:name2*&fq={SolrField.NAME_SELECT}:name3* OR {SolrField.NAME_STEM_AGRO}:name3*'),
+    ('test-complex-1', {'query': 'name1 name2 name3', 'fields': [SolrField.NAME_SELECT, SolrField.NAME_STEM_AGRO, SolrField.NAME_SYNONYM, SolrField.IDENTIFIER_SELECT, SolrField.BN_SELECT], 'wild': [SolrField.IDENTIFIER_SELECT, SolrField.BN_SELECT]}, f'q={SolrField.NAME_SELECT}:name1 OR {SolrField.NAME_STEM_AGRO}:name1 OR {SolrField.NAME_SYNONYM}:name1 OR {SolrField.IDENTIFIER_SELECT}:name1* OR {SolrField.BN_SELECT}:name1*&fq={SolrField.NAME_SELECT}:name2 OR {SolrField.NAME_STEM_AGRO}:name2 OR {SolrField.NAME_SYNONYM}:name2 OR {SolrField.IDENTIFIER_SELECT}:name2* OR {SolrField.BN_SELECT}:name2*&fq={SolrField.NAME_SELECT}:name3 OR {SolrField.NAME_STEM_AGRO}:name3 OR {SolrField.NAME_SYNONYM}:name3 OR {SolrField.IDENTIFIER_SELECT}:name3* OR {SolrField.BN_SELECT}:name3*'),
+])
+def test_build_split_query(test_name, params, expected):
+    """Assert that the build_split_query function works as expected."""
+    split_query = Solr.build_split_query(params['query'], params['fields'], params['wild'])
+    assert split_query == expected
+
+
+@pytest.mark.parametrize('test_name,query,names,expected', [
+    ('test-1', '1234567', ['1234567 B.C. LTD.'], ['<b>1234567</b> B.C. LTD.']),
+    ('test-2', 'my query', ['MY QUERY 1', 'THIS IS MY QUERY'], ['<b>MY QUERY</b> 1', 'THIS IS <b>MY QUERY</b>']),
+])
+def test_highlight_names(test_name, query, names, expected):
+    """Assert the highlight names function works as expected."""
+    highlighted_names = Solr.highlight_names(query.upper(), names)
+    assert highlighted_names == expected
+
+
+@pytest.mark.parametrize('test_name,facet_data,expected', [
+    ('test-1',
+     {'facet_counts':{'facet_fields': {SolrField.TYPE:['BEN',23,'CP',10,'SP',102],SolrField.STATE:['ACTIVE',23,'HISTORICAL',10]}}},
+     {'fields':{SolrField.TYPE:[{'value':'BEN','count':23},{'value':'CP','count':10},{'value':'SP','count':102}],SolrField.STATE:[{'value':'ACTIVE','count':23},{'value':'HISTORICAL','count':10}]}}),
+])
+def test_parse_facets(test_name, facet_data, expected):
+    """Assert the parse facets function works as expected."""
+    facet_info = Solr.parse_facets(facet_data)
+    assert facet_info == expected
