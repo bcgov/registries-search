f8ec9a6dc3e3e94a3f41718ea77bef2d8d3ff9c3
Kial Jinnah <kialjinnah@gmail.com>
2022-05-30 10:02:03 -0700

    renamed to AutoComplete for better review diff
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- search-ui/src/components/search/SearchBar.vue
+++ search-ui/src/components/search/SearchBar.vue
@@ -17,7 +17,7 @@
           @keydown="suggest.disabled=false"
           @keyup.enter="submitSearch()"
         />
-          <suggest-list v-if="suggestActive" />
+          <auto-complete v-if="suggestActive" />
       </v-col>
       <v-col class="pl-3 pt-2">
         <v-row no-gutters>
@@ -39,7 +39,7 @@
 import { computed } from 'vue'
 // local
 import { useSearch, useSuggest } from '@/composables';
-import { SuggestList } from '.'
+import { AutoComplete } from '.'
 
 // Composables
 const { suggest, suggestActive } = useSuggest()
--- search-ui/src/components/search/index.ts
+++ search-ui/src/components/search/index.ts
@@ -1,4 +1,4 @@
 
 export { default as SearchBar } from './SearchBar.vue'
 export { default as SearchResults } from './SearchResults.vue'
-export { default as SuggestList } from './SuggestList.vue'
+export { default as AutoComplete } from './AutoComplete.vue'
