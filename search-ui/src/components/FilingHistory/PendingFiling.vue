4859af305fb3f49241cd02b83fd75c9ce80528dd
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2022-05-30 17:33:55 -0700

    no message

--- search-ui/src/components/FilingHistory/PendingFiling.vue
+++ search-ui/src/components/FilingHistory/PendingFiling.vue
@@ -2,7 +2,7 @@
     <div v-if="props.filing" class="pending-filing-details body-2">
         <h4>Filing Pending</h4>
 
-        <p>This {{ title }} is paid, but the filing has not been completed by the Business Registry yet.
+        <p>This {{ title() }} is paid, but the filing has not been completed by the Business Registry yet.
             Some filings may take longer than expected.</p>
 
         <p v-if="props.filing.courtOrderNumber">Court Order Number: {{ props.filing.courtOrderNumber }}</p>
