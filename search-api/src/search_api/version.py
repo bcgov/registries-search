859c765c7611b02979942bd7809fd2573a3c2212
Kial <Kial.Jinnah@gov.bc.ca>
2022-08-03 10:49:31 -0700

    fixed solr call bug (#126)
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>
    
    Co-authored-by: Kial Jinnah <kialjinnah@gmail.com>

--- search-api/src/search_api/version.py
+++ search-api/src/search_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '1.0.0b1.dev2'  # pylint: disable=invalid-name
+__version__ = '1.0.0b1.dev3'  # pylint: disable=invalid-name
