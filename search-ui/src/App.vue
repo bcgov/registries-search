ac3358acc49054bff9e4f99d132a7ffd9c9270eb
Kial Jinnah <kialjinnah@gmail.com>
2022-08-16 11:30:24 -0700

    cleanup
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- search-ui/src/App.vue
+++ search-ui/src/App.vue
@@ -149,8 +149,7 @@ const handleError = (error: ErrorI) => {
   // FUTURE: add account info with error information, add dialog popups for specific errors
   switch (error.category) {
     case ErrorCategories.ACCOUNT_ACCESS:
-      console.log('here')
-      // navigate(sessionStorage.getItem('REGISTRY_URL'))
+      navigate(sessionStorage.getItem('REGISTRY_URL'))
   }
   Sentry.captureException(error)
 }
