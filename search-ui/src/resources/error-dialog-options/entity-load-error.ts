168f166027218df42c20a700028a05e3ae9bddb1
Kial <Kial.Jinnah@gov.bc.ca>
2022-09-28 16:31:36 -0700

    fixed error dialog fn (#160)
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>
    
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>
    Co-authored-by: Kial Jinnah <kialjinnah@gmail.com>

--- search-ui/src/resources/error-dialog-options/entity-load-error.ts
+++ search-ui/src/resources/error-dialog-options/entity-load-error.ts
@@ -1,17 +1,11 @@
-// external
-import { useRouter } from 'vue-router'
 // local
 import { DialogOptionsI } from '@/interfaces'
-import { RouteNames } from '@/enums'
-
-const redirectToSearch = () => {
-  const router = useRouter()
-  router.push({ name: RouteNames.SEARCH })
-}
+import { navigate } from '@/utils'
 
 export const EntityLoadError: DialogOptionsI = {
-  buttons: [{ onClick: redirectToSearch, onClickClose: true, text: 'OK' }],
-  onClose: redirectToSearch,
+  buttons: [{ onClick: navigate, onClickArgs: [sessionStorage.getItem('BASE_URL')], onClickClose: true, text: 'OK' }],
+  onClose: navigate,
+  onCloseArgs: [sessionStorage.getItem('BASE_URL')],
   text: 'The Business Search application is currently unable to ' +
     'load the dashboard for this business. Please try again later.',
   title: 'Business Dashboard unavailable'
