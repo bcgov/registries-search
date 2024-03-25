ee405e3c09eaca074c958ffa2e5d312c23201e5c
Kial <kialj876@gmail.com>
2024-03-25 15:00:15 -0400

    18717 small bug fix (#259)
    * keep periods in value query
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    * version update
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    ---------
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- search-api/src/search_api/services/solr/solr.py
+++ search-api/src/search_api/services/solr/solr.py
@@ -298,7 +298,7 @@ class Solr:
     def prep_query_str(query: str) -> str:
         """Return query string prepped for solr call."""
         # replace solr specific special chars
-        rmv_spec_chars_rgx = r'([\[\]!()\"~*?:/\\={}^%`#|<>,.@$;_\-])'
+        rmv_spec_chars_rgx = r'([\[\]!()\"~*?:/\\={}^%`#|<>,@$;_\-])'
         handled_spec_chars_rgx = r'([&+]+)'
         query = re.sub(rmv_spec_chars_rgx, ' ', query.lower())
         return re.sub(handled_spec_chars_rgx, r' \\\1 ', query) if not query.isspace() else r'\*'
--- search-api/src/search_api/version.py
+++ search-api/src/search_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '1.0.9.post1'  # pylint: disable=invalid-name
+__version__ = '1.0.10'  # pylint: disable=invalid-name
