35d84558022caaf30277a82a9bc095d47a4af3b0
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2024-03-25 12:40:31 -0700

    Adding detail info box for info items. (#245)

---
+++ bor-ui/src/components/base/DetailsInfoBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p v-if="title" class="title">
+      {{ title }}
+    </p>
+    <p v-if="content">
+      {{ content }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  content?: string
+}>()
+</script>
+
+<style lang="scss" scoped>
+.title {
+  letter-spacing: 0.05rem;
+  font-weight: bold;
+  font-size: smaller;
+  font-style: italic;
+}
+</style>
