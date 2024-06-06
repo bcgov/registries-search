6e32d726e1e94565611861691f29a40f17f6b2c9
Kial Jinnah <kialj876@gmail.com>
2024-06-06 15:07:04 -0700

    lowered webpack version
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- search-ui/vue.config.js
+++ search-ui/vue.config.js
@@ -5,39 +5,22 @@ process.env.VUE_APP_VERSION = process.env.npm_package_version
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    performance: {
-      hints: false
-    },
-    optimization: {
-      splitChunks: {
-        minSize: 10000,
-        maxSize: 250000,
-      }
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
-      })
-    ],
-    module: {
-      rules: [
-        {
-          use: [
-            {
-              loader: 'thread-loader'
-            },
-            {
-              loader: 'ts-loader',
-              options: {
-                happyPackMode: true
-              }
-            }
-          ]
-        },
-      ],
-    }
-  },
+  // configureWebpack: {
+  //   performance: {
+  //     hints: false
+  //   },
+  //   optimization: {
+  //     splitChunks: {
+  //       minSize: 10000,
+  //       maxSize: 250000,
+  //     }
+  //   },
+  //   plugins: [
+  //     new webpack.DefinePlugin({
+  //       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+  //     })
+  //   ]
+  // },
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
