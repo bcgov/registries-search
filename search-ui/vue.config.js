bb00b52e114f3a748a65e49df2d786a859cbf831
Kial Jinnah <kialj876@gmail.com>
2024-06-06 12:34:09 -0700

    attempt to fix build in gcp
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- search-ui/vue.config.js
+++ search-ui/vue.config.js
@@ -6,6 +6,15 @@ process.env.VUE_APP_VERSION = process.env.npm_package_version
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    },
     plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
