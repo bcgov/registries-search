da237bcd6ed7c96e7d2fd6ccee95c896eb0119b3
Kial <kialj876@gmail.com>
2025-05-27 17:16:30 -0400

    28429 too many nested clauses (#322)
    * API: updates for too many nested clauses
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    * chore: cleanup
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    ---------
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- search-api/src/search_api/services/business_solr/utils/business_search.py
+++ search-api/src/search_api/services/business_solr/utils/business_search.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Business search methods."""
+from search_api.exceptions import SolrException
 from search_api.services.base_solr.utils import QueryParams
 from search_api.services.business_solr import BusinessSolr
 from search_api.services.business_solr.doc_fields import BusinessField
@@ -33,6 +34,26 @@ def business_search(params: QueryParams, solr: BusinessSolr):
     initial_queries["query"] += f' OR ({BusinessField.IDENTIFIER_Q_EDGE.value}:"{params.query["value"]}"^5)'
 
     # add defaults
+    solr_payload = _get_solr_payload(params, solr, initial_queries)
+    try:
+        return solr.query(solr_payload, params.start, params.rows)
+    except SolrException as err:
+        if "Query contains too many nested clauses" in err.error:
+            # retry with a simpler query
+            simplified_queries = solr.query_builder.build_base_query(
+                query=params.query,
+                fields=params.query_fields,
+                boost_fields={},
+                fuzzy_fields={}
+            )
+            retry_payload = _get_solr_payload(params, solr, simplified_queries)
+            return solr.query(retry_payload, params.start, params.rows)
+        # else pass exception along
+        raise err
+
+
+def _get_solr_payload(params: QueryParams, solr: BusinessSolr, initial_queries: dict):
+    """Return the payload for the business search solr call."""
     solr_payload = {
         **initial_queries,
         "queries": {
@@ -57,5 +78,4 @@ def business_search(params: QueryParams, solr: BusinessSolr):
                          categories=params.child_categories,
                          is_nested=True,
                          solr=solr)
-
-    return solr.query(solr_payload, params.start, params.rows)
+    return solr_payload
