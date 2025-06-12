3c18f5ff6bc702ab4c821e6698241dcdfcd90be5
Kial <kialj876@gmail.com>
2025-06-12 13:00:46 -0400

    chore: update config (#330)

--- search-solr-importer/src/search_solr_importer/config.py
+++ search-solr-importer/src/search_solr_importer/config.py
@@ -71,9 +71,9 @@ class Config:
     IS_PARTIAL_IMPORT = not INCLUDE_COLIN_LOAD or not INCLUDE_LEAR_LOAD
 
     # Service account details
-    ACCOUNT_SVC_AUTH_URL = os.getenv("KEYCLOAK_AUTH_TOKEN_URL")
-    ACCOUNT_SVC_CLIENT_ID = os.getenv("BUSINESS_SEARCH_SERVICE_ACCOUNT_CLIENT_ID")
-    ACCOUNT_SVC_CLIENT_SECRET = os.getenv("BUSINESS_SEARCH_SERVICE_ACCOUNT_SECRET")
+    ACCOUNT_SVC_AUTH_URL = os.getenv("ACCOUNT_SVC_AUTH_URL")
+    ACCOUNT_SVC_CLIENT_ID = os.getenv("ACCOUNT_SVC_CLIENT_ID")
+    ACCOUNT_SVC_CLIENT_SECRET = os.getenv("ACCOUNT_SVC_CLIENT_SECRET")
     try:
         ACCOUNT_SVC_TIMEOUT = int(os.getenv("AUTH_API_TIMEOUT", "20"))
     except:  # pylint: disable=bare-except;
