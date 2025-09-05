deb784e48d03d6d17537265701761a1cf902be7c
Kial <kialj876@gmail.com>
2025-09-05 15:16:55 -0400

    25807 deployment config 3 (#378)
    * UI - fix redirect for comp auth user
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    * cleanup
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    ---------
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- bor-ui/app/utils/open-business.ts
+++ bor-ui/app/utils/open-business.ts
@@ -1,6 +1,6 @@
 export const goToOpenBusiness = (identifier: string, modernized = false) => {
   const { accessLevel } = useSearchAccessStore()
-  const { bconlineUrl, businessDashURL, registriesSearchUrl } = useRuntimeConfig().public
+  const { bconlineUrl, businessDashUrl, registriesSearchUrl } = useRuntimeConfig().public
 
   if (modernized) {
     if (accessLevel !== SearchAccess.EXTENDED) {
@@ -8,7 +8,7 @@ export const goToOpenBusiness = (identifier: string, modernized = false) => {
       useBcrosNavigate().redirect(registriesSearchUrl, { identifier }, '_blank')
     } else {
       // will redirect to business dashboard for given identifier
-      useBcrosNavigate().redirect(businessDashURL + identifier, {}, '_blank')
+      useBcrosNavigate().redirect(businessDashUrl + identifier, {}, '_blank')
     }
   } else {
     useBcrosNavigate().redirect(bconlineUrl, {}, '_blank')
